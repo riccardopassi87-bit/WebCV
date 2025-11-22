4eeb4debe2c2a0239a3a3c569c2141edaefc9e0b
riccardopassi87-bit <riccardo.passi87@gmail.com>
2025-11-22 14:55:22 +0100

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('english').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'index-en.html';
+});
+
+document.getElementById('german').addEventListener('click', function(event){
+    event.preventDefault();
+    window.location.href = 'index-de.html';
+});
