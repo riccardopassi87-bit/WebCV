e2aee5677e4c4b4e8e6a954f74fc965f1d0d1430
riccardopassi87-bit <riccardo.passi87@gmail.com>
2025-11-23 19:48:59 +0100

    comm

--- script.js
+++ script.js
@@ -14,19 +14,70 @@ $(document).ready(function () {
     $("[data-lang-group][lang='de']").show();
 });
 
-// POWER BUTTON
+// FLAG SYSTEM FOR ANIMATIONS
+
+let german = true;
+
+$('#german').on('click', function(){
+    german = true;
+})
+
+$('#english').on('click', function(){
+    german = false;
+})
+
+
 $('#power').show();
-$('#preload').hide();
 $('#monitor').hide();
+$('#navMenu').hide();
+$('#preload').hide();
 $('#languages').hide();
+
+// POWER BUTTON
 
 $('#power').on('click', function(){
     $('#power').toggle();
     $('#monitor').toggle();
     $('#preload').show();
+    
+    const elementId = german ? 'loadDe' : 'loadEn';
+    const systemReady = "h4[data-lang-group='start'][lang='" + (german ? "de" : "en") + "']";
+
+    const textToAnimate = $('#' + elementId).text();
+
+    $("[data-lang-group='start'][lang]").hide();
+    $('#' + elementId).show().text('');
+
+    charAnimation(elementId, textToAnimate, 25, function() {
+        setTimeout(function (){
+            $(systemReady).show();
+            setTimeout(function () {
+                $('#preload').hide();
+                $('#navMenu').show();
+            }, 2000);
+        }, 1000);
+    });    
 })
 
 $('#close-window').on('click', function(){
     $('#monitor').toggle();
     $('#power').toggle();
+    $('#navMenu').hide();
+    $('#preload').hide();
 })
+
+// 1 BY 1 CHAR ANIMATION
+function charAnimation(elementId, text, delay, onComplete){
+    let i = 0;
+
+    function type() {
+        if(i < text.length) {
+            $('#' + elementId).append(text.charAt(i));
+            i++;
+            setTimeout(type, delay);
+        } else {
+            if (onComplete) onComplete();
+        }
+    }
+    type();
+}
