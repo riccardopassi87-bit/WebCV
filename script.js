70c8ab090aad3a1c5c33fd8a3c58d71322a6f776
riccardopassi87-bit <riccardo.passi87@gmail.com>
2025-11-23 16:09:35 +0100

    comm

--- script.js
+++ script.js
@@ -14,15 +14,19 @@ $(document).ready(function () {
     $("[data-lang-group][lang='de']").show();
 });
 
-
 // POWER BUTTON
-
+$('#power').show();
 $('#preload').hide();
 $('#monitor').hide();
 $('#languages').hide();
 
 $('#power').on('click', function(){
-    $('#power').hide();
-    $('#monitor').show();
+    $('#power').toggle();
+    $('#monitor').toggle();
     $('#preload').show();
+})
+
+$('#close-window').on('click', function(){
+    $('#monitor').toggle();
+    $('#power').toggle();
 })
